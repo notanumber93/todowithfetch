99c9f97739fc94788d71e985d8a3b387354e52b4
Natalia <natalia.avivnotario@gmail.com>
2020-10-13 18:44:58 -0300

    todo con fetch sin asincronia

--- src/App.js
+++ src/App.js
@@ -1,26 +1,98 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { AiFillCloseCircle } from "react-icons/ai";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [task, setTask] = useState("");
+  const [taskList, setTaskList] = useState([]);
+  const updateList = async () => {
+    console.log(taskList);
+    await fetch("https://assets.breatheco.de/apis/fake/todos/user/natalia",
+    {
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "PUT",
+      body: JSON.stringify(taskList),
+    })
+    .then(response => response.text())
+    .then(newResponse => {
+      console.log(newResponse);
+    })
+    .catch(error => console.log(error))
+  }
 
-export default App;
+  const handleTaskList = async (e) => {
+    e.preventDefault()
+    const taskNew =  [
+      ...taskList,
+      { label: task, done: false}
+    ]
+    setTaskList(
+      [
+        ...taskNew
+      ]
+    )
+    await updateList();
+  }
+
+  const getList = () => {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/natalia",
+      {
+        headers: {
+          "Content-Type": "application/json"
+        },
+        method: "GET",
+  
+      })
+
+      .then(response => response.text())
+      .then(newResponse => {
+        setTaskList(JSON.parse(newResponse))
+        console.log(newResponse);
+      })
+      .catch(error => console.log(error))
+  }
+
+
+  useEffect(() => {
+    getList();
+  }, [])
+
+  
+  
+  const removeTask = (index) => {  
+      const filterData = taskList.filter(item => item !== taskList[index]);
+      setTaskList(filterData);
+  }
+
+    return (
+
+      <div className="App">
+        <div className="container notebook">
+          <h1 className="title display-3">to do list</h1>
+          <form onSubmit={(e) => {handleTaskList(e)}}>
+            <input className="form-control input-group-lg"
+              placeholder="New Task" 
+              onChange={e => setTask(e.target.value)}/>
+          </form>
+          <ul >
+            {taskList.map((item, index) => (
+              <li className="list-group-item d-flex justify-content-between align-items-center"
+                key={index} >
+                {item.label} <AiFillCloseCircle  onClick={() => {removeTask(index)}}  />
+              </li>
+            ))}
+            <div>
+              <small>
+                {taskList.length} &nbsp; Item left
+            </small>
+            </div>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+
+  export default App;
